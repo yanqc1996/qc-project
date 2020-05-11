6f15e9e2f15cfd58c0511979ae3f6c4ade5791cc
Yanqc <qincheng.yan@huanuo-nsb.com>
2020-05-11 15:57:16 +0800

    项目首页动作提交

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,24 +1,39 @@
 <template>
-  <div>
-    
-  </div>
+    <div class='qc-container'>
+        <div v-for="(item,index) in 100" :key='index' class='qc-test'>
+            <div v-for="(item,index) in 100" :key='index' class='qc-test'>
+                <img :src='data' class='qc-test'>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 /**
-* @description
-*/
+ * @description
+ */
 export default {
-  components: {},
-  mounted(){},
-  data() {
-    return {
-      
-    }
-  },
-  methods:{},
+    components: {},
+    mounted() {
+        this.data = this.test[0]
+        setTimeout(() => {
+           this.data=this.test[1] //黑名单框清除
+        }, 10000)
+    },
+    data() {
+        return {
+            test: [require('@assets/login/login-bg.jpg'), require('@assets/login/login-left.png')],
+            data: ''
+        }
+    },
+    methods: {}
 }
 </script>
 
-<style scoped>
+<style lang='less'>
+.qc-test {
+    width: 50px;
+    height: 50px;
+    border: 1px solid black;
+}
 </style>
--- src/views/main/Index.vue
+++ src/views/main/Index.vue
@@ -1,22 +1,23 @@
 <template>
-  <div>
-    
-  </div>
+    <div class='qc-container'>1
+        <!-- <div v-for="(item,index) in 100" :key='index' style='width:10px;height:10px；border:1px solid black'>
+            <div v-for="(item,index) in 100" :key='index' style='width:10px;height:10px;border:1px solid black'>
+            </div>
+        </div> -->
+    </div>
 </template>
 
 <script>
 /**
-* @description
-*/
+ * @description
+ */
 export default {
-  components: {},
-  mounted(){},
-  data() {
-    return {
-      
-    }
-  },
-  methods:{},
+    components: {},
+    mounted() {},
+    data() {
+        return {}
+    },
+    methods: {}
 }
 </script>
 
